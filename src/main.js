c94d758d441077177479e9c03a1f22010c6ab88a
readyWwq <1570769179@qq.com>
2018-12-01 11:18:42 +0800

    111

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUi from'element-ui'//引入element-ui
+import 'element-ui/lib/theme-chalk/index.css'//手动引入element-ui 的css
+import axios from 'axios'
+import Qs from 'qs'
 Vue.config.productionTip = false
-
+Vue.use(ElementUi)//调用element-ui
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/tool/api/apiurl.js
@@ -0,0 +1,3 @@
+export default{
+  baseURL : '/api', //设置默认基础地址 location.origin
+}
