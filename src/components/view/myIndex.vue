de7456c7f845797ced49e988a5f1c9e623e112c3
unknown <1570769179@qq.com>
2019-05-18 18:03:01 +0800

    123

--- src/components/view/myIndex.vue
+++ src/components/view/myIndex.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     主页开发中
+    <el-input v-model="input" placeholder=""></el-input>
+    <el-button type="发送邮件" @click="send">发送邮件</el-button>
   </div>
 </template>
 <script>
 export default {
   name: "",
-  data: () => ({
+  data(){
+    return{
+      input:'1948074943@qq.com',
+    }
+  },
+  methods: {
+    send() {
+      this.$post(this.$api.sendEmail, {input:this.input}).then((data) => {
 
-  })
+      });
+    }
+  }
+
 }
 </script>
 <style scoped>
--- src/tool/api/apiurl.js
+++ src/tool/api/apiurl.js
@@ -2,7 +2,7 @@ const baseURL = ''+'/api' //设置默认基础地址  /api  location.origin http
 export default {
   upload: baseURL + '/upload',
   getCode: baseURL + '/getCode', //取得验证码
-  sendEmail: baseURL + '/sendEmail' //发送验证码
+  sendEmail: baseURL + '/sendEmail', //发送验证码
 
   login: baseURL + '/login', //登陆
   addUser: baseURL + '/addUser', //添加用户
