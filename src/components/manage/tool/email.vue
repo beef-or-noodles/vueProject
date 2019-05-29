6ce5b1c3dfbd93bd91088283805defa75acbae73
readyWwq <1570769179@qq.com>
2019-05-29 20:27:04 +0800

    123

--- src/components/manage/tool/email.vue
+++ src/components/manage/tool/email.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-
+    <el-input v-model="input" placeholder=""></el-input>
+    <el-button type="发送邮件" @click="send">发送邮件</el-button>
   </div>
 </template>
 <script>
 export default {
   data(){
     return{
-
+      input:'1948074943@qq.com',
     }
   },
-  created() {
-
+  methods:{
+    send(){
+      this.$post(this.$api.sendEmail, {emailId:this.input}).then((data) => {});
+    },
   }
 }
 </script>
