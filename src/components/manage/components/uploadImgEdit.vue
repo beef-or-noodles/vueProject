de2dbdc2742db3fb59a6a9b4762be8af4345e767
dev2 <1570769179@qq.com>
2019-08-05 17:08:15 +0800

    添加图片剪裁插件

---
+++ src/components/manage/components/uploadImgEdit.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="copper">
+            <vueCropper
+                    ref="cropper"
+                    :img="option.img"
+                    :outputSize="option.size"
+                    :outputType="option.outputType"
+                    :fixedBox="option.fixedBox"
+                    :centerBox="option.centerBox"
+            ></vueCropper>
+        </div>
+        <div class="btnBox">
+            <el-button type="primary"></el-button>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import {VueCropper} from "vue-cropper";
+
+    export default {
+        name: "uploadImgEdit",
+        components: {
+            VueCropper
+        },
+        data() {
+            return {
+                option: {
+                    img: "",
+                    outputType: "png",
+                    fixedBox: true,
+                    centerBox: true,
+                }
+            }
+        },
+        methods: {
+            name() {
+
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
