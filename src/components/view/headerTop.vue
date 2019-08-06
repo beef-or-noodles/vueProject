1594aecce5545b5120ea904ff6ad8a9ae8550e5d
dev2 <1570769179@qq.com>
2019-08-06 15:45:59 +0800

    添加图片剪裁插件

--- src/components/view/headerTop.vue
+++ src/components/view/headerTop.vue
@@ -7,16 +7,16 @@
       <h2 id="mnavh"><span class="navicon"></span></h2>
       <ul id="starlist">
         <li>
-          <router-link :to="{ path: `/${userID}/myIndex`}" active-class="active">网站首页</router-link>
+          <router-link :to="{ path: `/myIndex`}" active-class="active">网站首页</router-link>
         </li>
         <li>
-          <router-link :to="{ path: `/${userID}/photo`}" active-class="active">相册</router-link>
+          <router-link :to="{ path: `/photo`}" active-class="active">相册</router-link>
         </li>
         <li>
-          <router-link :to="{ path: `/${userID}/content?id=2`}">关于我</router-link>
+          <router-link :to="{ path: `/content?id=2`}">关于我</router-link>
         </li>
         <li>
-          <router-link :to="{ path: `/${userID}/login`}" active-class="active" target="_blank">登录</router-link>
+          <router-link :to="{ path: `/login`}" active-class="active" target="_blank">登录</router-link>
         </li>
       </ul>
     </nav>
